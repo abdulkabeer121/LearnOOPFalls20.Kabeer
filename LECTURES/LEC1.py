b7f5d1ddf716464e9798afe6068c0841284a7b14
abdulkabeer121 <kabeer.abid419@gmail.com>
2020-09-29 07:38:06 -0700

    LAB 1 -----> PRINT STATEMENT

---
+++ LECTURES/LEC1.py
@@ -0,0 +1,31 @@
+#---------------------------
+# How to print a statement
+#---------------------------
+
+print ("My name is abdul kabeer abid")
+print ("My age is 19 years")
+print ("I am studing Bechelors in Data Science")
+print ("My Rollno is BDSM-S20-005")
+
+#---------------------------
+# How to print a veriable
+#---------------------------
+
+name = "Abdullah"
+print ("\n")
+print (name)
+
+#-----------------------------------------------------------------
+# How to print a statement and veriable in a same print statement
+#-----------------------------------------------------------------
+
+username = "Abdulkabeerabid"
+age = 19
+course = "Bechelors in Data Science"
+rollNo = "BDSM-S20-005"
+
+print ("\nMy name is " + username)
+print ("My age is " + str(age) + " years")
+print ("I am studing " + course)
+print ("My Roll no is " + rollNo)
+
